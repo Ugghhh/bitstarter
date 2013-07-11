3b0e0ad637eae3fa339e8964d0ab6cf6811b3916
Ugghhh <angelowo@umich.edu>
2013-07-11 01:10:38 -0600

    Completes hw3 with shitty solution to sidestepping asynchronous problem; just duplicated code and put it in the same scope so that get request is done and then the checks are done

--- grader.js
+++ grader.js
@@ -22,16 +22,19 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var URL_DEFAULT = "http://pure-escarpment-4079.herokuapp.com/";
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -49,8 +52,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +64,37 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var getURLdata = function(url,callback) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {console.error('Error ' + util.format(response.message));}
+	else {
+	    fs.writeFileSync('temp.html',result);
+	    var chkJson = callback('temp.html',program.checks);
+	    var outJson = JSON.stringify(chkJson, null, 4);
+	    console.log(outJson);
+	    fs.unlink('temp.html');
+	}
+    });
+
+};
+
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'Path to URL')
+	.parse(process.argv);
+    var fileToCheck;
+    if (program.url !== undefined) {
+	getURLdata(program.url,checkHtmlFile);
+    } else {
+	var checkJson = checkHtmlFile( program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
